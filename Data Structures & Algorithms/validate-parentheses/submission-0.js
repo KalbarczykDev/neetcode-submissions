63ed6069b9afcc7d41f1e2d7c232419652ac2569
Oskar Kalbarczyk <106467480+KalbarczykDev@users.noreply.github.com>
2026-07-12 23:58:07 +0200

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s) {
+        const pairs = { ")": "(", "]": "[", "}": "{" };
+        const stack = [];
+
+        for (const char of s) {
+            if (char === "(" || char === "[" || char === "{") {
+                stack.push(char);
+            } else {
+                if (stack.pop() !== pairs[char]) return false;
+            }
+        }
+
+        return stack.length === 0;
+    }
+}
