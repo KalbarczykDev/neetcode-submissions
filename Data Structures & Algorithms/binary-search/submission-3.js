54f7f00a88f0a34183829decdb30289c53b79cba
Oskar Kalbarczyk <106467480+KalbarczykDev@users.noreply.github.com>
2026-07-22 14:58:52 +0200

    Add: binary-search - submission-3

---
+++ Data Structures & Algorithms/binary-search/submission-3.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    search(nums, target) {
+        let l = 0;
+        let r = nums.length - 1;
+        let m;
+        while (l <= r) {
+            m = Math.floor((l + r) / 2);
+            if (nums[m] > target) {
+                r  = m - 1;
+            } else if (nums[m] < target) {
+                l = m  + 1;
+            } else {
+                return m;
+            }
+        }
+
+        return -1;
+    }
+}
