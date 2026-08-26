a6a10fbe4c95f70bdfb54f8b4dc12ed8328d296d
Oskar Kalbarczyk <106467480+KalbarczykDev@users.noreply.github.com>
2026-08-26 10:09:04 +0200

    Add: clone-graph - submission-0

---
+++ Data Structures & Algorithms/clone-graph/submission-0.java
@@ -0,0 +1,47 @@
+/*
+Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    public Node cloneGraph(Node node) {
+        if (node == null) return null;
+
+        Map<Node,Node> clones = new HashMap<>();
+
+        Node newNode = dfs(node,clones);
+
+        return newNode;
+    }
+
+
+    private Node dfs(Node node, Map<Node,Node> clones){
+        if(clones.containsKey(node)){
+            return clones.get(node);
+        }
+
+        Node clone = new Node(node.val);
+        clones.put(node, clone);
+
+        for(Node neighbor : node.neighbors){
+            clone.neighbors.add(dfs(neighbor, clones));
+        }
+        
+        return clone;
+    }
+}
