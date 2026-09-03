d43edb69dd768f471994fb58409b19298fee6b00
Oskar Kalbarczyk <106467480+KalbarczykDev@users.noreply.github.com>
2026-09-03 15:19:23 +0200

    Add: counting-bits - submission-0

---
+++ Data Structures & Algorithms/counting-bits/submission-0.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] countBits(int n) {
+        
+        int[] res = new int [n + 1];
+
+        for(int i = 0; i <= n; i++){
+            int cur = i;
+
+            while(cur != 0){
+
+                if((cur & 1) == 1){
+                    res[i]++;
+                }
+
+                cur = cur >>> 1;
+            }
+
+        }
+
+        return res;
+
+    }
+}
